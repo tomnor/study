8a49cb52fcb6a1e834884f5b0dcb10a57725e455
tomnor <tomasn@kth.se>
2014-12-26 22:39:42 +0100

    Synch remote, the sound thing just drafts

--- sound.py
+++ sound.py
@@ -2,6 +2,7 @@
 """
 Play with the sox library (not a python thing)
 """
+import numpy as np
 
 def write_soxdat(fo, rate, *data):
     """Write data to opened file f.
@@ -17,15 +18,51 @@ def write_soxdat(fo, rate, *data):
         The sampling rate.
 
     data: array-likes
-        One or more array-like objects to write. The first one is the
-        time 'column'.
+        One or more array-like objects to write. Those are the
+        "channels" in the .dat file.
 
     """
     width, spacecnt = 16, 1
-    # fmt = '{:15f}  '
     fmt = '{:' + str(width) + 'f}' + spacecnt * ' '
-    fmt = len(data) * fmt + '\n'
-    lines = [fmt.format(*tup) for tup in zip(*data)]
+    fmt = (len(data) + 1) * fmt + '\n'
+    T = np.linspace(0, 1.0 / rate * len(data[0]), num=len(data[0]),
+                    endpoint=False)
+    lines = [fmt.format(*tup) for tup in zip(T, *data)]
 
-    # return lines
+    header = ('; Sample Rate ' + str(rate) + '\n' +
+              '; Channels {}\n'.format(len(data)))
+    fo.write(header)
     fo.writelines(lines)
+
+def adjust_normalize(a, tozero=np.mean, fromto=(10, 90)):
+    """Return a version of array a, offset so that the magnitude
+    produced by tozero is zero. Also make peak values be between +/-
+    1. Base the conversion on fromto of the array, (percentage).
+
+    a: numpy 1d array
+        ...
+
+    tozero: func
+        ...
+
+    fromto: tuple
+        ...
+
+    """
+
+    fromto = slice(int(fromto[0] * 0.01 * len(a)),
+                   int(fromto[1] * 0.01 * len(a)))
+    base = tozero(a[fromto])
+    a = a - base
+    peak = max(abs(min(a[fromto])), abs(max(a[fromto])))
+    x = 1.0 / peak
+    a = a * x
+    return a
+
+
+# Given a sine wave described with 50 points and a desired freqency of
+# 500 Hz:
+# p = 1 / f = 1 / 500 = 0.002     # period
+# pq = p / 50 = 0.002 / 50 = 4 x 10^(-5) (0.00004) # time increment per sample
+# # point
+# rate = 1 / pq = 1 / 0.00004 = 25000              # 25 kHz
